2da539d4245046b4d60542c481ca6910f9ea7ad1
TNDKien <kienn770@gmail.com>
2024-09-24 22:33:32 +0200

    switching tailwind to traditional css
    tailwind is some confusing shi- stuff

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,56 +2,38 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 import "../app/[locale]/globals.css";
 
 const Header = ({ blok }) => (
-  <div className="blok-feature bg-DarkGreen shadow-md fixed w-full z-10">
-    <div
-      className="container mx-auto px-4 py-3 flex justify-between items-center"
-      {...storyblokEditable(blok)}
-    >
+  <div className="header-container" {...storyblokEditable(blok)}>
+    <div className="header-content">
       {/* Logo */}
       <img
-        className="h-10 w-auto"
+        className="logo"
         src={blok.logo.filename}
         alt={blok.logo.alt || "Logo"}
       />
 
       {/* Navigation */}
-      <nav className="flex">
-        <ul className="flex space-x-6 items-center bg-grey rounded-full px-6 py-2 ml-2">
+      <nav className="navigation">
+        <ul className="navigation-list">
           {/* About Us */}
           <li>
-            <a
-              href="/about-us"
-              className="text-OffWhite px-4 py-2 hover:bg-DarkTeal rounded-full"
-            >
+            <a href="/about-us" className="nav-link">
               About Us
             </a>
           </li>
 
           {/* Services Dropdown */}
-          <li className="relative group">
-            <a
-              href="#"
-              className="text-OffWhite px-4 py-2 hover:bg-DarkTeal rounded-full"
-            >
+          <li className="dropdown">
+            <a href="#" className="nav-link">
               Services
             </a>
-            <div className="absolute hidden group-hover:block bg-LimeGreen shadow-lg py-2 mt-1 space-y-1 w-48">
-              <a
-                href="/service-1"
-                className="block px-4 py-2 text-OffWhite hover:bg-LightLime"
-              >
+            <div className="dropdown-menu">
+              <a href="/service-1" className="dropdown-item">
                 Service 1
               </a>
-              <a
-                href="/service-2"
-                className="block px-4 py-2 text-OffWhite hover:bg-LightLime"
-              >
+              <a href="/service-2" className="dropdown-item">
                 Service 2
               </a>
-              <a
-                href="/service-3"
-                className="block px-4 py-2 text-OffWhite hover:bg-LightLime"
-              >
+              <a href="/service-3" className="dropdown-item">
                 Service 3
               </a>
             </div>
@@ -59,20 +41,14 @@ const Header = ({ blok }) => (
 
           {/* Pricing */}
           <li>
-            <a
-              href="/pricing"
-              className="text-OffWhite px-4 py-2 hover:bg-DarkTeal rounded-full"
-            >
+            <a href="/pricing" className="nav-link">
               Pricing
             </a>
           </li>
 
           {/* Training */}
           <li>
-            <a
-              href="/training"
-              className="text-OffWhite px-4 py-2 hover:bg-DarkTeal rounded-full"
-            >
+            <a href="/training" className="nav-link">
               Training
             </a>
           </li>
@@ -80,10 +56,7 @@ const Header = ({ blok }) => (
       </nav>
 
       {/* Contact Us */}
-      <a
-        href="/contact-us"
-        className="ml-4 px-4 py-2 rounded-full text-DarkGreen bg-Pink hover:bg-LimeGreen"
-      >
+      <a href="/contact-us" className="contact-button">
         Contact Us
       </a>
     </div>
