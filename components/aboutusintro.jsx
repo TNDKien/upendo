db63cb4380b686ca2e5174dcb3dfecf8463d6635
MisterSaji <164487904+MisterSaji@users.noreply.github.com>
2024-09-27 15:57:36 +0200

    vandaag laatste wijzigingen

--- components/aboutusintro.jsx
+++ components/aboutusintro.jsx
@@ -9,25 +9,34 @@ const AboutUsIntro = ({ blok }) => (
 
     <div className="grotemiauw">
       <div className="aboutusintrotext">
-        <p>Our top priority is making data</p>
+        <p>
+          Our top priority is making<span className="zeng"> data</span>
+        </p>
+      </div>{" "}
+      <div className="aboutusintrocolor"></div>
+      {/* Tijdelijk de lege div verwijderd */}
+      {/* <div className="whitetext"> </div> */}
+      <div className="introtext">
+        <p className="simpleText">
+          simple<span className="white"> and</span>
+        </p>
+        <p className="simpleText">
+          accessible<span className="white">.</span>
+        </p>
       </div>
     </div>
 
-    <div className="aboutusintrocolor"></div>
-    <p className="simpleText">
-      simple<span className="white"> and</span>
-    </p>
-
-    {/* Tijdelijk de lege div verwijderd */}
-    {/* <div className="whitetext"> </div> */}
-
-    <div className="simpleText">
-      <p>
-        accessible<span className="white">.</span>
-      </p>
-    </div>
     <div className="aboutusend">
       <p>Data simplified.</p>
+    </div>
+    <div className="visual">
+      <div className="icon-holder">
+        <img
+          className="logo"
+          src={blok.visualfoto.filename}
+          alt={blok.visualfoto.alt || "visual"}
+        />
+      </div>
     </div>
   </div>
 );
