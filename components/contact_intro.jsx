aa6b041800a7f19212b442adb34ac63421e537af
Bo Hofland <bo.hofland@student.fontys.nl>
2024-09-27 14:31:39 +0200

    contact styling

--- components/contact_intro.jsx
+++ components/contact_intro.jsx
@@ -5,10 +5,12 @@ import "../app/[locale]/contact.css";
 const contact_intro = ({ blok }) => (
   <div className="contact-intro" {...storyblokEditable(blok)}>
     <p className="accent">{blok.contact_accent}</p>
-    <h2>{blok.contact_titel}</h2>
-    <h2>{blok.contact_paragraph}</h2>
-    <a href={blok.linkedin.url}>linkje</a>
-    <p>{blok.contact_contact}</p>
+    <h2 className="title">{blok.contact_titel}</h2>
+    <h2 className="paragraph">{blok.contact_paragraph}</h2>
+    <a  href={blok.linkedin.url}>
+        <p className="link">LinkedIn</p>
+    </a>
+    <p className="contact">{blok.contact_contact}</p>
   </div>
 );
 
