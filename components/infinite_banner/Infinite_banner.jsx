d5aa3ed9b5d4ca905034cb0dda2c36b0e4830a36
TNDKien <kienn770@gmail.com>
2024-10-03 15:31:56 +0200

    banner commit

--- components/infinite_banner/Infinite_banner.jsx
+++ components/infinite_banner/Infinite_banner.jsx
@@ -8,8 +8,22 @@ const Infinite_banner = ({ blok }) => {
     setMenuOpen(!menuOpen);
   };
 
-  return;
-  <div></div>;
+  return (
+    <div className="logo-slider relative border border-gray-500 p-5 overflow-hidden max-w-[900px] w-full mx-auto flex">
+      <div className="logo-track flex justify-around animate-marquee">
+        <img src={blok.banner_image.filename} className="" />
+        <img src={blok.banner_image.filename} className="" />
+        <img src={blok.banner_image.filename} className="" />
+        <img src={blok.banner_image.filename} className="" />
+      </div>
+      <div className="logo-track flex justify-around animate-marque">
+        <img src={blok.banner_image.filename} className="" />
+        <img src={blok.banner_image.filename} className="" />
+        <img src={blok.banner_image.filename} className="" />
+        <img src={blok.banner_image.filename} className="" />
+      </div>
+    </div>
+  );
 };
 
 export default Infinite_banner;
