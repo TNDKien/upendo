ae065571b234ed10d5a2517a38f18fe6baff14ea
TNDKien <kienn770@gmail.com>
2024-10-11 16:02:24 +0200

    servicecards desktop init

--- components/home_content/Home_service_intro.jsx
+++ components/home_content/Home_service_intro.jsx
@@ -3,24 +3,30 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 
 const Home_service_intro = ({ blok }) => {
   return (
-    <div className="flex flex-col gap-36 pt-32" {...storyblokEditable(blok)}>
-      <div className="flex flex-col p-6 gap-1">
-        <h2 className="text-4xl text-offWhite max-w-64">{blok.service_text}</h2>
-        <a
-          className="flex mt-10 py-2 px-6 w-auto bg-darkTeal text-pink rounded-3xl self-start whitespace-nowrap"
-          href=""
-        >
-          See all our services
-        </a>
-      </div>
-      <div className="flex flex-col ml-auto p-8">
+    <div
+      className="flex flex-col gap-36 pt-32 lg:p-32 lg:pb-0"
+      {...storyblokEditable(blok)}
+    >
+      <div className="flex flex-col lg:flex-row lg:justify-between">
+        <div className="flex flex-col p-6 gap-1 lg:justify-center">
+          <h2 className="text-2xl text-offWhite w-72 lg:text-2xl">
+            {blok.service_text}
+          </h2>
+          <a
+            className="flex mt-10 py-2 px-6 w-auto bg-darkTeal text-pink rounded-3xl self-start whitespace-nowrap"
+            href=""
+          >
+            See all our services
+          </a>
+        </div>
         <img
-          className=""
+          className="p-6 ml-auto lg:w-1/2 lg:mr-0 lg:p-0"
           src={blok.image.filename}
           alt={blok.image.alt || ""}
         />
       </div>
-      <span className="flex mr-auto p-6 pb-0 text-pink text-xs font-mono">
+
+      <span className="flex mr-auto p-6 pb-0 text-pink text-xs font-mono lg:text-xl">
         {blok.alt_text}
       </span>
     </div>
--- components/strapi/Strapi.jsx
+++ components/strapi/Strapi.jsx
@@ -15,7 +15,7 @@ const Strapi = ({ blok }) => {
       scrollTimeout = setTimeout(() => {
         if (containerRef.current && contentRef.current) {
           const containerTop = containerRef.current.getBoundingClientRect().top;
-          const totalPictures = blok.pictures.length;
+          const totalPictures = blok.pictures?.length || 0;
 
           // Get the total height of the content (e.g., the div with paragraphs)
           const contentHeight = contentRef.current.scrollHeight;
@@ -51,22 +51,25 @@ const Strapi = ({ blok }) => {
     >
       {/* Sticky image container */}
       <div className="sticky top-[25vh] h-[50vh] w-1/2 flex justify-center items-center">
-        {blok.pictures && blok.pictures.length > 0 && (
-          <img
-            src={blok.pictures[currentIndex].filename}
-            alt={
-              blok.pictures[currentIndex].alt || `Picture ${currentIndex + 1}`
-            }
-            style={{
-              transform:
-                currentIndex % 2 === 0
-                  ? "translateX(50vw) rotateY(-15deg) rotateX(10deg)"
-                  : "translateX(0vw) rotateY(15deg) rotateX(10deg)",
-              transition:
-                "transform 0.6s cubic-bezier(0.68, -0.55, 0.27, 1.55)",
-            }}
-          />
-        )}
+        {blok.pictures &&
+          blok.pictures.length > 0 &&
+          blok.pictures[currentIndex] && (
+            <img
+              className="w-3/4"
+              src={blok.pictures[currentIndex].filename}
+              alt={
+                blok.pictures[currentIndex].alt || `Picture ${currentIndex + 1}`
+              }
+              style={{
+                transform:
+                  currentIndex % 2 === 0
+                    ? "translateX(50vw) rotateY(-15deg) rotateX(10deg)"
+                    : "translateX(0vw) rotateY(15deg) rotateX(10deg)",
+                transition:
+                  "transform 0.6s cubic-bezier(0.68, -0.55, 0.27, 1.55)",
+              }}
+            />
+          )}
       </div>
 
       {/* Scrolling content */}
@@ -74,11 +77,91 @@ const Strapi = ({ blok }) => {
         ref={contentRef}
         className="relative flex flex-col gap-[110vh] z-10 text-offWhite p-40 pt-0 -mt-72"
       >
-        <p className="flex flex-end w-40">{blok.config_text}</p>
-        <p className="flex flex-end w-40 ml-auto mr-0">{blok.config_text}</p>
-        <p className="flex flex-end w-40">{blok.config_text}</p>
-        <p className="flex flex-end w-40 ml-auto mr-0">{blok.config_text}</p>
-        <p className="flex flex-end w-40">{blok.config_text}</p>
+        <div className="flex flex-col w-60">
+          <h3 className={`flex flex-grow flex-row items-end -ml-2`}>
+            <span className="text-6xl mr-2">&#9687;</span>
+            <span className="text-2xl mb-1">Configuration</span>
+          </h3>
+          <p>{blok.config_text}</p>
+          <div className="flex items-center text-base">
+            <p className={`z-10 p-3 px-6 m-0 rounded-full bg-darkTeal`}>
+              See more
+            </p>
+            <div
+              className={` text-darkTeal -ml-10 pl-12 pr-4 pb-2 pt-1 rounded-r-full text-3xl`}
+            >
+              &rarr;
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col w-60 ml-auto mr-0">
+          <h3 className={`flex flex-grow flex-row items-end -ml-2`}>
+            <span className="text-6xl mr-2">&#9687;</span>
+            <span className="text-2xl mb-1">Configuration</span>
+          </h3>
+          <p>{blok.config_text}</p>
+          <div className="flex items-center text-base">
+            <p className={`z-10 p-3 px-6 m-0 rounded-full bg-darkTeal`}>
+              See more
+            </p>
+            <div
+              className={` text-darkTeal -ml-10 pl-12 pr-4 pb-2 pt-1 rounded-r-full text-3xl`}
+            >
+              &rarr;
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col w-60">
+          <h3 className={`flex flex-grow flex-row items-end -ml-2`}>
+            <span className="text-6xl mr-2">&#9687;</span>
+            <span className="text-2xl mb-1">Configuration</span>
+          </h3>
+          <p>{blok.config_text}</p>
+          <div className="flex items-center text-base">
+            <p className={`z-10 p-3 px-6 m-0 rounded-full bg-darkTeal`}>
+              See more
+            </p>
+            <div
+              className={` text-darkTeal -ml-10 pl-12 pr-4 pb-2 pt-1 rounded-r-full text-3xl`}
+            >
+              &rarr;
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col w-60 ml-auto mr-0">
+          <h3 className={`flex flex-grow flex-row items-end -ml-2`}>
+            <span className="text-6xl mr-2">&#9687;</span>
+            <span className="text-2xl mb-1">Configuration</span>
+          </h3>
+          <p>{blok.config_text}</p>
+          <div className="flex items-center text-base">
+            <p className={`z-10 p-3 px-6 m-0 rounded-full bg-darkTeal`}>
+              See more
+            </p>
+            <div
+              className={` text-darkTeal -ml-10 pl-12 pr-4 pb-2 pt-1 rounded-r-full text-3xl`}
+            >
+              &rarr;
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col w-60">
+          <h3 className={`flex flex-grow flex-row items-end -ml-2`}>
+            <span className="text-6xl mr-2">&#9687;</span>
+            <span className="text-2xl mb-1">Configuration</span>
+          </h3>
+          <p>{blok.config_text}</p>
+          <div className="flex items-center text-base">
+            <p className={`z-10 p-3 px-6 m-0 rounded-full bg-darkTeal`}>
+              See more
+            </p>
+            <div
+              className={` text-darkTeal -ml-10 pl-12 pr-4 pb-2 pt-1 rounded-r-full text-3xl`}
+            >
+              &rarr;
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
