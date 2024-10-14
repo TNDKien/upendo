92e80b9e9a7aef2a127c3cd68d74b33c2a1a7ecb
hanlyvt <hanlytrang@gmail.com>
2024-10-14 13:40:48 +0200

    service-responsive

--- components/service/servicebody.jsx
+++ components/service/servicebody.jsx
@@ -3,17 +3,13 @@ import { render } from "storyblok-rich-text-react-renderer";
 import "./servicebody.css";
 
 const servicebody = ({ blok }) => (
-    <div {...storyblokEditable(blok)}>
-        <div className="servicebodytitel">
-            {render(blok.servicebodytitel)}
-        </div>
-        <div className="servicebodytekst1">
-            {render(blok.servicebodytekst1)}
-        </div>
-        <div className="servicebodytekst2">
-            {render(blok.servicebodytekst2)}
-        </div>
+  <div {...storyblokEditable(blok)}>
+    <div className="servicebodytitel">{render(blok.servicebodytitel)}</div>
+    <div className="servicecontainer">
+      <div className="servicebodytekst1">{render(blok.servicebodytekst1)}</div>
+      <div className="servicebodytekst2">{render(blok.servicebodytekst2)}</div>
     </div>
+  </div>
 );
 
 export default servicebody;
