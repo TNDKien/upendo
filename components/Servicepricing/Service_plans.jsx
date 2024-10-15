49e2210598914b10660fc73698c0f8e6243c25c5
hanlyvt <hanlytrang@gmail.com>
2024-10-15 18:26:19 +0200

    service plans responsive

--- components/Servicepricing/Service_plans.jsx
+++ components/Servicepricing/Service_plans.jsx
@@ -6,12 +6,14 @@ const service_plans = ({ blok }) => (
   <div {...storyblokEditable(blok)}>
     <div className="serviceplanstitel">{render(blok.service_plans_titel)}</div>
     <div className="plans_container">
-      {/* Herhaal je blokken hier */}
       <div className="serviceplanstitle1">
         {render(blok.service_plans_titel1)}
       </div>
+
       <div className="service_plans_text_opsomming">
+        {/* Visual */}
         <div className="service_lijntje"></div>
+        {/* De verschillende punten */}
         <div className="serviceplanstext1">
           {render(blok.service_plans_text1)}
         </div>
