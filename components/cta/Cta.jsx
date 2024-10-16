ac19949ea1836df7c66b1f420c4a44215aa27b0e
TNDKien <kienn770@gmail.com>
2024-10-16 23:24:39 +0200

    Update Cta.jsx
    made it responsive

--- components/cta/Cta.jsx
+++ components/cta/Cta.jsx
@@ -8,28 +8,29 @@ const Cta = ({ blok }) => {
       {...storyblokEditable(blok)}
     >
       <img
-        className="h-[300px] -mr-20 z-0"
+        className="h-[300px] -ml-24 lg:ml-auto lg:-mr-20 z-0"
         src={blok.visual.filename}
         alt={blok.visual.alt}
       />
       <img
-        className="h-[300px] rounded-tr-[50px]"
+        className="hidden lg:block h-[300px] rounded-tr-[50px]"
         src={blok.image.filename}
         alt={blok.image.alt}
       />
 
-      <div className="flex flex-col justify-between p-12 gap-12 border-y border-r border-limeGreen w-full">
+      <div className="flex flex-col -ml-20 lg:ml-0 justify-between p-6 lg:p-12 gap-12 border-y border-r border-limeGreen w-full">
         <p className="font-area font-bold text-2xl text-offWhite">
           {blok.text}
         </p>
-
-        {blok.button &&
-          blok.button.map((nestedBlok) => {
-            if (nestedBlok.component === "button") {
-              return <Button blok={nestedBlok} key={nestedBlok._uid} />;
-            }
-            return null;
-          })}
+        <div className="self-end lg:self-start">
+          {blok.button &&
+            blok.button.map((nestedBlok) => {
+              if (nestedBlok.component === "button") {
+                return <Button blok={nestedBlok} key={nestedBlok._uid} />;
+              }
+              return null;
+            })}
+        </div>
       </div>
     </div>
   );
