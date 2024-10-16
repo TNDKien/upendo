6aaba26c1498e6c7f0a4ba133aad9aef4bdb8816
koenderaden <koenderaden@gmail.com>
2024-10-16 09:12:28 +0200

    kleine fix trainingcards
    kleine fix groottes

--- components/training/trainingcard.jsx
+++ components/training/trainingcard.jsx
@@ -14,45 +14,65 @@ const TrainingCard = ({ blok }) => {
 
   return (
     <div className="trainingcard">
-      <div style={{ padding: "24px" }}>
-        {/* Render de Training Header */}
-        {blok.training_header && (
-          <div className="training_header">{render(blok.training_header)}</div>
-        )}
-
-        {/* Nieuw blok: Training Image Left */}
+      <div className="card_content">
+        {/* Left Image Layout */}
         {blok.training_image_left && blok.training_image_left.filename && (
-          <div className="training_image_left" data-aos="fade-right">
-            <img
-              className="training_image"
-              src={blok.training_image_left.filename}
-              alt="training image left"
-            />
+          <div className="left_image_layout">
+            <div className="training_image_left" data-aos="fade-right">
+              <img
+                className="training_image"
+                src={blok.training_image_left.filename}
+                alt="training image left"
+              />
+            </div>
+            <div className="text_content">
+              {blok.training_header && (
+                <div className="training_header">
+                  {render(blok.training_header)}
+                </div>
+              )}
+              {blok.training_paragraph && (
+                <div className="training_paragraph">
+                  {render(blok.training_paragraph)}
+                </div>
+              )}
+              {blok.schedule_training && (
+                <div className="schedule_training">
+                  {render(blok.schedule_training)}
+                </div>
+              )}
+            </div>
           </div>
         )}
 
-        {/* Nieuw blok: Training Image Right */}
+        {/* Right Image Layout */}
         {blok.training_image_right && blok.training_image_right.filename && (
-          <div className="training_image_right" data-aos="fade-left">
-            <img
-              className="training_image"
-              src={blok.training_image_right.filename}
-              alt="training image right"
-            />
-          </div>
-        )}
-
-        {/* Render de Training Paragraph */}
-        {blok.training_paragraph && (
-          <div className="training_paragraph">
-            {render(blok.training_paragraph)}
-          </div>
-        )}
-
-        {/* Render de Training Schedule */}
-        {blok.schedule_training && (
-          <div className="schedule_training">
-            {render(blok.schedule_training)}
+          <div className="right_image_layout">
+            <div className="text_content">
+              {blok.training_header && (
+                <div className="training_header">
+                  {render(blok.training_header)}
+                </div>
+              )}
+              {blok.training_paragraph && (
+                <div className="training_paragraph">
+                  {render(blok.training_paragraph)}
+                </div>
+              )}
+              {blok.schedule_training && (
+                <div className="schedule_training">
+                  {render(blok.schedule_training)}
+                </div>
+              )}
+            </div>
+            
+            <div className="training_image_right" data-aos="fade-left">
+              <img
+                className="training_image"
+                src={blok.training_image_right.filename}
+                alt="training image right"
+              />
+            </div>
           </div>
         )}
       </div>
