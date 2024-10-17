536e18c981049ba3ad780d3ee2f9c16f9c501e02
TNDKien <kienn770@gmail.com>
2024-10-17 22:44:34 +0200

    Update Howweserve.jsx

--- components/howweserve/Howweserve.jsx
+++ components/howweserve/Howweserve.jsx
@@ -4,7 +4,7 @@ import { render } from "storyblok-rich-text-react-renderer";
 const Howweserve = ({ blok }) => {
   return (
     <div {...storyblokEditable(blok)}>
-      <div className="w-80 font-mono py-6">{render(blok.howtext)}</div>
+      <div className="w-80 font-mono py-6 text-xl">{render(blok.howtext)}</div>
     </div>
   );
 };
