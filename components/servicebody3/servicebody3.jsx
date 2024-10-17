2327ca59ea801407a2c2be0124047be38d6f7c9e
TNDKien <kienn770@gmail.com>
2024-10-17 20:48:26 +0200

    renaming components based on vercel error

--- components/servicebody3/servicebody3.jsx
+++ components/servicebody3/servicebody3.jsx
@@ -2,25 +2,22 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 import { render } from "storyblok-rich-text-react-renderer";
 import "./servicebody3.css";
 
-const servicebody3 = ({ blok }) => (
-    <div {...storyblokEditable(blok)}>
-        <div className="servicebodyheader">
-            {render(blok.servicebodyheader)}
-        </div>
-        <div className="servicebodyheadertekst">
-            {render(blok.servicebodyheadertekst)}
-        </div>
-        <div className="servicebodyheadertekst2">
-            {render(blok.servicebodyheadertekst2)}
-        </div>
-        <div className="servicebodyheadertekst3">
-            {render(blok.servicebodyheadertekst3)}
-        </div>
-        <div className="servicepricing_title">
-            {render(blok.servicepricing_title)}
-        </div>
+const Servicebody3 = ({ blok }) => (
+  <div {...storyblokEditable(blok)}>
+    <div className="servicebodyheader">{render(blok.servicebodyheader)}</div>
+    <div className="servicebodyheadertekst">
+      {render(blok.servicebodyheadertekst)}
     </div>
-
+    <div className="servicebodyheadertekst2">
+      {render(blok.servicebodyheadertekst2)}
+    </div>
+    <div className="servicebodyheadertekst3">
+      {render(blok.servicebodyheadertekst3)}
+    </div>
+    <div className="servicepricing_title">
+      {render(blok.servicepricing_title)}
+    </div>
+  </div>
 );
 
-export default servicebody3;
+export default Servicebody3;
