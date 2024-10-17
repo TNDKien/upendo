e312f06f723a072478531c9b152d622ad7e5ec49
TNDKien <kienn770@gmail.com>
2024-10-17 20:42:30 +0200

    Update Accordion.jsx

--- components/accordion/Accordion.jsx
+++ components/accordion/Accordion.jsx
@@ -9,15 +9,18 @@ const Accordion = ({ blok }) => {
   };
 
   return (
-    <div className="w-1/2 bg-pink" {...storyblokEditable(blok)}>
+    <div
+      className="mx-32 mb-2 rounded-xl w-1/2 bg-pink text-darkGreen"
+      {...storyblokEditable(blok)}
+    >
       <button
-        className="accordion w-1/2 text-left py-4 px-6 border-none outline-none transition duration-300 cursor-pointer"
+        className="accordion w-full text-left py-4 px-6 border-none outline-none transition duration-300 cursor-pointer"
         onClick={() => toggleAccordion(0)}
       >
         {blok.question}
       </button>
       <div
-        className={`panel w-1/2 px-6 overflow-hidden transition-max-height duration-300 ease-in-out ${
+        className={`panel w-full px-6 overflow-hidden transition-max-height duration-300 ease-in-out ${
           activeIndex === 0 ? "max-h-full" : "max-h-0"
         }`}
       >
