6f7a7b138d2ab0946e8c1173dfd269eb39624746
TNDKien <kienn770@gmail.com>
2024-10-17 18:22:01 +0200

    Update Accordion.jsx

--- components/accordion/Accordion.jsx
+++ components/accordion/Accordion.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { storyblokEditable } from "@storyblok/react/rsc";
 
-const Accordion = () => {
+const Accordion = ({ blok }) => {
   const [activeIndex, setActiveIndex] = useState(null);
 
   const toggleAccordion = (index) => {
@@ -11,13 +11,13 @@ const Accordion = () => {
   return (
     <div className="w-1/2 bg-pink" {...storyblokEditable(blok)}>
       <button
-        className="accordion w-1/2 text-left py-4 px-6  border-none outline-none transition duration-300 cursor-pointer"
+        className="accordion w-1/2 text-left py-4 px-6 border-none outline-none transition duration-300 cursor-pointer"
         onClick={() => toggleAccordion(0)}
       >
         {blok.question}
       </button>
       <div
-        className={`panel w-1/2 px-6 overflow-hidden  transition-max-height duration-300 ease-in-out ${
+        className={`panel w-1/2 px-6 overflow-hidden transition-max-height duration-300 ease-in-out ${
           activeIndex === 0 ? "max-h-full" : "max-h-0"
         }`}
       >
