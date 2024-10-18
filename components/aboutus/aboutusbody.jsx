f3bfe7156259bb7decce51a223c32aa33fc673ba
MisterSaji <164487904+MisterSaji@users.noreply.github.com>
2024-10-18 11:06:02 +0200

    about us title fix

--- components/aboutus/aboutusbody.jsx
+++ components/aboutus/aboutusbody.jsx
@@ -2,7 +2,10 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 import { render } from "storyblok-rich-text-react-renderer";
 
 const aboutusbody = ({ blok }) => (
-  <div className="flex flex-col p-6 gap-2 lg:w-80" {...storyblokEditable(blok)}>
+  <div
+    className="flex flex-col p-6 gap-2 lg:w-80 lg:p-0 lg:mr-24"
+    {...storyblokEditable(blok)}
+  >
     <img
       className="w-6 h-6"
       src={blok.visualfototwee.filename}
--- components/aboutus/aboutusintro.jsx
+++ components/aboutus/aboutusintro.jsx
@@ -2,16 +2,16 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 import "./aboutusintro.css";
 import "./aboutusintrotext.css";
 import "./aboutusintrocolor.css";
-import { render } from "storyblok-rich-text-react-renderer";
 
 const AboutUsIntro = ({ blok }) => (
   <div {...storyblokEditable(blok)} className="aboutus-container">
     <div className="aboutussubtext">{blok.aboutussubtext}</div>
 
-    <div className="grotemiauw">
+    <div className="titelblok">
       <div className="aboutusintrotext">
         <p>
-          Our top priority is making<span className="zeng"> data</span>
+          Our top priority is making
+          <span className="zeng"> data</span>
         </p>
       </div>
       <div className="aboutusintrocolor"></div>
