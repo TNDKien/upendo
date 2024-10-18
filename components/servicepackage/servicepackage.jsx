263249983079732efa110b008929d3c25722822f
TNDKien <kienn770@gmail.com>
2024-10-18 09:55:15 +0200

    merge fix

--- components/servicepackage/servicepackage.jsx
+++ components/servicepackage/servicepackage.jsx
@@ -3,37 +3,10 @@ import { render } from "storyblok-rich-text-react-renderer";
 import "./servicepackage.css";
 
 const servicepackage = ({ blok }) => (
-    <div {...storyblokEditable(blok)}>
-        <div className="flexboxrow2">
-            <div className="flexboxfiguur"></div>
-            <div className="flexboxrowtekst">
-                {render(blok.flexboxrowtekst)}
-            </div>
-        </div>
-        <div className="flexboxrow2">
-            <div className="flexboxfiguur"></div>
-            <div className="flexboxrowtekst">
-                {render(blok.flexboxrowtekst2)}
-            </div>
-        </div>
-        <div className="flexboxrow2">
-            <div className="flexboxfiguur"></div>
-            <div className="flexboxrowtekst">
-                {render(blok.flexboxrowtekst3)}
-            </div>
-        </div>
-        <div className="flexboxrow2">
-            <div className="flexboxfiguur"></div>
-            <div className="flexboxrowtekst">
-                {render(blok.flexboxrowtekst4)}
-            </div>
-        </div>
-        <div className="flexboxrow2">
-            <div className="flexboxfiguur"></div>
-            <div className="flexboxrowtekst_bottom">
-                {render(blok.flexboxrowtekst5)}
-            </div>
-        </div>
+  <div {...storyblokEditable(blok)}>
+    <div className="flexboxrow2">
+      <div className="flexboxfiguur"></div>
+      <div className="flexboxrowtekst">{render(blok.flexboxrowtekst)}</div>
     </div>
     <div className="flexboxrow2">
       <div className="flexboxfiguur"></div>
@@ -49,7 +22,9 @@ const servicepackage = ({ blok }) => (
     </div>
     <div className="flexboxrow2">
       <div className="flexboxfiguur"></div>
-      <div className="flexboxrowtekst">{render(blok.flexboxrowtekst5)}</div>
+      <div className="flexboxrowtekst_bottom">
+        {render(blok.flexboxrowtekst5)}
+      </div>
     </div>
   </div>
 );
